8c60c2b4ed4979983651dc4a7d0b6a39a4031a7b
Daf <dafne.azzolina@gmail.com>
2022-09-21 12:11:26 -0300

    Add controller specs for users and posts

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /id' do
-    it 'returns http success' do
-      get '/users/id'
-      expect(response).to have_http_status(:success)
+  context 'Correct response status' do
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/users'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET /show' do
+      it 'returns http success' do
+        get '/users/:user_id'
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+  context 'Correct template' do
+    describe 'GET users' do
+      it 'renders the index template' do
+        get '/users'
+        expect(response).to render_template(:index)
+        expect(response.body).to include('Here is a list of all users')
+      end
+      it 'renders the show template' do
+        get '/users/:user_id'
+        expect(response).to render_template(:show)
+        expect(response.body).to include('Here is a user given an id')
+      end
     end
   end
 end
