b96de09a965d94301a4bc12fac22840c2f099808
Daf <dafne.azzolina@gmail.com>
2022-09-21 18:07:54 -0300

    Finish comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,10 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author: @user).order(id: :asc)
-    # @comments = Post.find_comments
   end
 
   def show
-    # @comments = Post.find_comments
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
