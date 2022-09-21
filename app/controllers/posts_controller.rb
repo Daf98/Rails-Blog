b2e39d19f4876dd23c8fc9dbb51ee06f2b6a58fa
Daf <dafne.azzolina@gmail.com>
2022-09-21 17:34:17 -0300

    Add post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,11 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.where(author: @user).order(id: :asc)
+    # @comments = Post.find_comments
   end
 
   def show
-
+    # @comments = Post.find_comments
+    @post = Post.find(params[:id])
   end
 end
