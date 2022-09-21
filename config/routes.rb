267940279b8a48fc48a833c52454e9711dcc614c
Daf <dafne.azzolina@gmail.com>
2022-09-21 16:10:09 -0300

    Add users route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get 'posts/show'
   # get 'users/id'
   # get 'users/show'
-  root 'pages#homepage'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :show]
