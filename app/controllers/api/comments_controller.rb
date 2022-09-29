7fb0cce2fdfec9f2df81ad3cf6cb33511e4a7f4a
Daf <dafne.azzolina@gmail.com>
2022-09-29 14:52:18 -0300

    Add user posts API endpoint

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,43 @@
+class Api::CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: @comment } }
+    end
+  end
+
+  def create
+    @comment = Comment.new(strong_params)
+    @post = Post.find(params[:post_id])
+    @user = current_user
+    @comment.author = current_user
+    @comment.post_id = @post.id
+    respond_to do |format|
+      format.html do
+        if @comment.save
+          flash[:success] = 'Comment saved successfully'
+          redirect_to user_post_url(@user, @post)
+        else
+          flash.now[:error] = 'Error: Comment could not be saved'
+        end
+      end
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to user_post_path
+  end
+
+  private
+
+  # Using a private method to encapsulate the permissible parameters
+  # is just a good pattern since you'll be able to reuse the same
+  # permit list between create and update. Also, you can specialize
+  # this method with per-user checking of permissible attributes.
+  def strong_params
+    params.require(:comment).permit(:title, :text)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,19 @@ Rails.application.routes.draw do
   end
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   resources :users, only: [:index, :show] do
   resources :posts, only: [:index, :new, :create, :show, :destroy] do
     resources :comments, only: [:new, :create, :destroy]
     resources :likes, only: [:create]
   end
+
+  # API endpoints
+  namespace :api do
+    resources :users, only: [:index] do
+      resources :posts, only: [:index], format: :json do
+        resources :comments, only: [:index, :create], format: :json
+      end
+    end
   end
+end
 end
